ac10ce1e0e2032136cf449abc5d20562d689857b
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2024-03-15 14:49:09 +0000

    Solution Season-1/Level-1

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -16,18 +16,39 @@ from collections import namedtuple
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+
+MAX_PAYMENT_AMOUNT = 1e5
+MIN_PAYMENT_AMOUNT = -MAX_PAYMENT_AMOUNT
+MAX_ITEM_AMOUNT = 2e4
+MIN_ITEM_AMOUNT = 0
+MAX_ITEM_QUANTITY = 100
+MIN_ITEM_QUANTITY = 0
+MAX_TOTAL_AMOUNT = 1e4
+MIN_TOTAL_AMOUNT = -MAX_TOTAL_AMOUNT
+
 def validorder(order: Order):
     net = 0
-
     for item in order.items:
+   
         if item.type == 'payment':
-            net += item.amount
+            if MIN_PAYMENT_AMOUNT < item.amount < MAX_PAYMENT_AMOUNT:
+                net += item.amount
+            else:
+                print("Payment not valid: %s" % item.amount)
+                continue
         elif item.type == 'product':
+            if not MIN_ITEM_QUANTITY <= item.quantity <= MAX_ITEM_QUANTITY:
+                print("Item quantity not valid: %s" % item.quantity)
+                continue
+            if not MIN_ITEM_AMOUNT < item.amount < MAX_ITEM_AMOUNT:
+                print("Item amount not valid: %s" % item.amount)
+                continue
             net -= item.amount * item.quantity
         else:
             return "Invalid item type: %s" % item.type
-
-    if net != 0:
+    if not MIN_TOTAL_AMOUNT < net < MAX_TOTAL_AMOUNT:
+        return "Total amount payable for an order exceeded"
+    if abs(net) >= 0.01:
         return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
     else:
         return "Order ID: %s - Full payment received!" % order.id
